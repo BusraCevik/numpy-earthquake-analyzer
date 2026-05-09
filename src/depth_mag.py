582d03d8525e28fde38d4a80c80c1b7e0b523306
Busra Cevik <busrazcvik@outlook.com>
2026-05-09 13:43:40 +0200

    not necessary imports removed

--- src/depth_mag.py
+++ src/depth_mag.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
 def plot_depth_vs_magnitude_hist(depths, magnitudes, output_path=None):
-    import matplotlib.pyplot as plt
     plt.figure(figsize=(10,6))
     plt.hist2d(depths, magnitudes, bins=[70, 50], range=[[0,70],[magnitudes.min(), magnitudes.max()]],
                cmap='magma_r', vmin=1, vmax=200)
@@ -11,12 +10,11 @@ def plot_depth_vs_magnitude_hist(depths, magnitudes, output_path=None):
     plt.ylabel('Magnitude')
     plt.title('2D Histogram: Magnitude vs Depth (0-100 km)')
     if output_path:
-        plt.savefig(output_path, bbox_inches='tight')  # output klasörüne kaydet
+        plt.savefig(output_path, bbox_inches='tight')
     plt.show()
 
 
 def plot_depth_vs_magnitude_scatter(depths, magnitudes, output_path=None):
-    import matplotlib.pyplot as plt
     plt.figure(figsize=(10, 6))
     plt.scatter(depths, magnitudes, alpha=0.5, s=10, c='#C71585')
     plt.xlabel('Depth (km)')
